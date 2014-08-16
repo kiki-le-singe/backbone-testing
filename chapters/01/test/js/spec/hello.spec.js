0024a267c1086146247b9d977f12c25c18fab290
kiki-le-singe <aalbertini95@gmail.com>
2014-08-16 22:42:27 +0200

    test(hello.spec.js): implements tests using 'Should'

--- chapters/01/test/js/spec/hello.spec.js
+++ chapters/01/test/js/spec/hello.spec.js
@@ -10,15 +10,26 @@ describe("Trying out the test libraries", function () {
     it("should be equal using 'expect'", function () {
       expect(hello()).to.equal("Hello World");
     });
+    it("should be equal using 'should'", function () {
+      hello().should.equal('Hello World');
+    });
   });
 
   describe("Sinon.JS", function () {
-    it("should report spy called", function () {
+    it("should report spy called using 'expect'", function () {
       var helloSpy = sinon.spy(window, 'hello');
 
       expect(helloSpy.called).to.be.false;
       hello();
       expect(helloSpy.called).to.be.true;
+      hello.restore();
+    });
+    it("should report spy called using 'should'", function () {
+      var helloSpy = sinon.spy(window, 'hello');
+
+      helloSpy.called.should.to.be.false;
+      hello();
+      helloSpy.called.should.to.be.true;
       hello.restore();
     });
   });
