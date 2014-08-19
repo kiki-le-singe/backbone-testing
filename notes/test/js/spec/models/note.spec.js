83f9a7f6abea6c7e9246a83126b2ebfcc6b89f29
kiki-le-singe <aalbertini95@gmail.com>
2014-08-19 23:31:52 +0200

    feat(*.spec.js): adds tests using the 'should' method

--- notes/test/js/spec/models/note.spec.js
+++ notes/test/js/spec/models/note.spec.js
@@ -9,6 +9,16 @@ describe("App.Models.Note", function () {
     expect(model.get("createdAt")).to.be.a("Date");
   });
 
+  it("has default values using 'should'", function () {
+    // Create empty note model.
+    var model = new App.Models.Note();
+
+    model.should.to.be.ok;
+    model.get("title").should.to.equal("");
+    model.get("text").should.to.equal("*Edit your note!*");
+    model.get("createdAt").should.to.be.a("Date");
+  });
+
   it("sets passed attributes", function () {
     var model = new App.Models.Note({
       title: "Grocery List",
@@ -17,5 +27,15 @@ describe("App.Models.Note", function () {
 
     expect(model.get("title")).to.equal("Grocery List");
     expect(model.get("text")).to.equal("* Milk\n* Eggs\n*Coffee");
+  });
+
+  it("sets passed attributes using 'should'", function () {
+    var model = new App.Models.Note({
+      title: "Grocery List",
+      text: "* Milk\n* Eggs\n*Coffee"
+    });
+
+    model.get("title").should.to.equal("Grocery List");
+    model.get("text").should.to.equal("* Milk\n* Eggs\n*Coffee");
   });
 });
--- notes/test/js/spec/namespace.spec.js
+++ notes/test/js/spec/namespace.spec.js
@@ -9,8 +9,22 @@ describe("Namespace", function () {
       "Routers", "Templates", "Views"
     );
   });
+  it("provides the 'App' object using 'should'", function () {
+    // Expect exists and is an object.
+    App.should.be.an('object');
+
+    // Expect all namespace properties are present.
+    App.should.include.keys(
+      "Config", "Collections", "Models",
+      "Routers", "Templates", "Views"
+    );
+  });
 
   it("provides the 'app' object", function () {
     expect(app).to.be.an("object");
+  });
+
+  it("provides the 'app' object using 'should'", function () {
+    app.should.to.be.an("object");
   });
 });
