33d9c1ca1c3d705bcb7095f76e825ddd60bce50e
kiki-le-singe <aalbertini95@gmail.com>
2014-08-21 00:21:40 +0200

    feat(chapters.03.chai-errors.spec.js): implements chai.should method

--- chapters/03/test/js/spec/chai-errors.spec.js
+++ chapters/03/test/js/spec/chai-errors.spec.js
@@ -18,4 +18,24 @@ describe("Chai errors", function () {
     expect(bad).to.throw(/message/);
     expect(bad).not.to.throw("no message match");
   });
+
+  it("SHOULD: checks `throw` - exception", function () {
+    var bad = function () {
+      throw new Error("My error message");
+    };
+
+    // Updated for Chai v1.9.0. Was:
+    //
+    //     expect(bad)
+    //       .to.throw(Error).and
+    //       .to.throw(/message/).and
+    //       .not.to.throw("no message match");
+    //
+    // The main issue is that Chai changed behavior to switch context
+    // on `throw()` calls to the Error object, which changed how
+    // chaining works. The below is equivalent.
+    bad.should.throw(Error);
+    bad.should.throw(/message/);
+    bad.should.not.throw("no message match");
+  });
 });
