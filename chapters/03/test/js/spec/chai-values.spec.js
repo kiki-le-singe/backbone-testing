22b57a207199f7b6a6e4256ae9db19d840060299
kiki-le-singe <aalbertini95@gmail.com>
2014-08-20 23:35:16 +0200

    feat(chapters.03.chai-valuess.spec.js): implements chai.should method

--- chapters/03/test/js/spec/chai-values.spec.js
+++ chapters/03/test/js/spec/chai-values.spec.js
@@ -6,6 +6,13 @@ describe("Chai value properties", function () {
     expect(false).to.not.be.ok;
   });
 
+  // Value is truthy.
+  it("checks `ok` using 'should'", function () {
+    "foo".should.be.ok;
+    true.should.be.ok;
+    false.should.not.be.ok;
+  });
+
   // Value is not `null` or `undefined`.
   it("checks `exist`", function () {
     expect("").to.exist;
@@ -14,10 +21,23 @@ describe("Chai value properties", function () {
     expect(undefined).to.not.exist;
   });
 
+  // Value is not `null` or `undefined`.
+  it("checks `exist` using 'should'", function () {
+    "".should.exist;
+    false.should.exist;
+    // null and undefined tests doens't work with 'should'
+  });
+
   it("checks `true`", function () {
     expect("foo").to.not.be.true;
     expect(true).to.be.true;
     expect(false).to.not.be.true;
+  });
+
+  it("checks `true` using 'should'", function () {
+    "foo".should.not.be.true;
+    true.should.be.true;
+    false.should.not.be.true;
   });
 
   it("checks `false`", function () {
@@ -26,10 +46,21 @@ describe("Chai value properties", function () {
     expect(false).to.be.false;
   });
 
+  it("checks `false` using 'should'", function () {
+    "".should.not.be.false;
+    true.should.not.be.false;
+    false.should.be.false;
+  });
+
   it("checks `null`", function () {
     expect(undefined).to.not.be.null;
     expect(false).to.not.be.null;
     expect(null).to.be.null;
+  });
+
+  it("checks `null` using 'should'", function () {
+    false.should.not.be.null;
+    // null and undefined tests doens't work with 'should'
   });
 
   it("checks `undefined`", function () {
@@ -38,8 +69,18 @@ describe("Chai value properties", function () {
     expect(null).to.not.be.undefined;
   });
 
+  it("checks `undefined` using 'should'", function () {
+    false.should.not.be.undefined;
+    // null and undefined tests doens't work with 'should'
+  });
+
   it("checks `arguments`", function () {
     expect(arguments).to.be.arguments;
     expect([]).to.not.be.arguments;
+  });
+
+  it("checks `arguments` using 'should'", function () {
+    arguments.should.be.arguments;
+    [].should.not.be.arguments;
   });
 });
