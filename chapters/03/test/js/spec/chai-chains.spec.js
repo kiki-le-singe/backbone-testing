4dfffca2f4195ba104a76d2a23a69e20b5b7814a
kiki-le-singe <aalbertini95@gmail.com>
2014-08-20 23:13:37 +0200

    feat(chapters.03.chai-chains.spec.js): implements chai.should method

--- chapters/03/test/js/spec/chai-chains.spec.js
+++ chapters/03/test/js/spec/chai-chains.spec.js
@@ -1,4 +1,15 @@
 describe("Chai assertion chains", function () {
+
+  // Runs once before all tests start.
+  before(function(){
+    this.foo = 'foo';
+  });
+
+  // Runs once when all tests finish.
+  after(function(){
+    this.foo = null;
+  });
+
   // A contrived example using all of the available Chai
   // language chains:
   // * `to`
@@ -12,24 +23,38 @@ describe("Chai assertion chains", function () {
   // * `at`
   // * `of`
   it("can chain assertions", function () {
-    expect("foo").a("string");
-    expect("foo").to.be.a("string");
-    expect("foo").to.have.been.a("string");
-    expect("foo").that.is.a("string");
+    expect(this.foo).a("string");
+    expect(this.foo).to.be.a("string");
+    expect(this.foo).to.have.been.a("string");
+    expect(this.foo).that.is.a("string");
 
     // Chains can be repeated.
-    expect("foo").to.to.to.to.a("string");
+    expect(this.foo).to.to.to.to.a("string");
 
     // OK, so we needed to use up all the rest here. ;)
-    expect("foo").and.with.at.of.a("string");
+    expect(this.foo).and.with.at.of.a("string");
+  });
+
+  it("can chain assertions using 'should'", function () {
+    // var foo = this.foo;
+    this.foo.should.be.a("string");
+    this.foo.should.have.been.a("string");
   });
 
   // `.not` can negate any assertion.
   it("can negate chains", function () {
-    expect("foo").to.not.equal("bar");
+    expect(this.foo).to.not.equal("bar");
 
     // Let's get literary.
     expect("Hamlet").to.be.not.to.be.an("object");
+  });
+
+  // `.not` can negate any assertion.
+  it("can negate chains using 'should'", function () {
+    this.foo.should.not.be.equal("bar");
+
+    // Let's get literary.
+    this.foo.should.not.be.an("object");
   });
 
   // `.deep` turns on deep equality checking.
@@ -43,12 +68,33 @@ describe("Chai assertion chains", function () {
       .to.have.deep.property("foo.bar", "baz");
   });
 
+  // `.deep` turns on deep equality checking.
+  it("can deeply check chains using 'should'", function () {
+    var foo = {foo: "bar"};
+    var foo2 = {foo: {bar: "baz"}};
+    // Without deep checking.
+    foo.should.not.be.equal({foo: "bar"});
+
+    // With deep checking.
+    foo.should.have.deep.equal({foo: "bar"});
+    foo2.should.have.deep.property("foo.bar", "baz");
+  });
+
   // Most Chai assertions are chainable
   it("can chain other assertions", function () {
-    expect("foo")
+    expect(this.foo)
       .to.be.a("string").and
       .to.equal("foo").and
       .to.have.length(3).and
       .to.match(/f[o]{2}/);
+  });
+
+  // Most Chai assertions are chainable
+  it("can chain other assertions using 'should'", function () {
+    this.foo.should
+      .be.a('string').and
+      .be.equal("foo").and
+      .have.length(3).and
+      .match(/f[o]{2}/);
   });
 });
